0ff34c9eb2f571815485d4e2a7061bc6d41e476c
tse00thomas <tse00thomas@gmail.com>
2022-02-15 16:57:23 +0800

    start renewing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
 import Header from "./components/Header";
 import Calendar from "./components/Calendar";
 import CalendarState from "./context/CalendarContext";
 import TaskForm from "./components/TaskForm";
 
 function App() {
-  
   return (
     <div className="container">
       <CalendarState>
         <Header />
         <Calendar />
-        <TaskForm/>
+        <TaskForm />
       </CalendarState>
-      <a href="https://github.com/sergiss/task-calendar" target="_blank"><i className="fa">&#xf09b;</i> Source Code </a>
     </div>
   );
 }
